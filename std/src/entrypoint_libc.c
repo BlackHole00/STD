9ce141590083d12c44dcd22cb8e89b563968fd37
BlackHole00 <lastapocalypse9@gmail.com>
2025-02-06 17:06:06 +0100

    STD: Make the libc entrypoint not take arguments

--- std/src/entrypoint_libc.c
+++ std/src/entrypoint_libc.c
@@ -1,8 +1,9 @@
-#ifdef STD_PLATFORM_LIBC
+#include <std/base/constant_symbols.h>
+#if defined(STD_PLATFORM_LIBC) || defined(STD_COMPILER_TCC)
 
 #include "entrypoint.h"
 
-int main(int argc, char** argv) {
+int main(void) {
 	std_main();
 
 	return 0;
