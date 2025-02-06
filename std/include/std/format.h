13b7dd9c0fe8f30d3a13b44e9d1504d7405acf7e
BlackHole00 <lastapocalypse9@gmail.com>
2025-02-06 17:02:28 +0100

    STD: Temporary improvements to format.h

--- std/include/std/format.h
+++ std/include/std/format.h
@@ -1,9 +1,14 @@
 #ifndef _STD_FORMAT_INCLUDED
 #define _STD_FORMAT_INCLUDED
 
-#include <std/constant_symbols.h>
-#include <std/string.h>
-#include <std/syscalls.h>
+/* TODO: This entire file is temporary, implement a stream and printing system */
+
+#include <std/base/constant_symbols.h>
+#include <std/base/string.h>
+
+#include <std/posix/syscalls.h>
+#include <std/windows/constants.h>
+#include <std/windows/kernel32.h>
 
 #ifdef STD_LANGUAGE_CPP
 extern "C" {
@@ -21,22 +26,44 @@ void print_bytes(void* data, usize size) {
 		for (j = 0; j < 8; j++) {
 			byte current_bit = (current_byte & mask) != 0;
 
-			std_syscall_write(0, &"01"[current_bit], 1);
+#ifdef STD_PLATFORM_POSIX
+			std_posix_syscall_write(0, &"01"[current_bit], 1);
+#elif defined(STD_PLATFORM_WINDOWS)
+			WriteConsoleA(GetStdHandle(STD_WINDOWS_STD_OUTPUT_HANDLE), &"01"[current_bit], 1, (std_LPDWORD)null, null);
+#endif
 			mask >>= 1;
 		}
 	}
 }
 
 void print_char(char character) {
-	std_syscall_write(0, &character, 1);
+#ifdef STD_PLATFORM_POSIX
+	std_posix_syscall_write(0, &character, 1);
+#elif defined(STD_PLATFORM_WINDOWS)
+	WriteConsoleA(GetStdHandle(STD_WINDOWS_STD_OUTPUT_HANDLE), &character, 1, (std_LPDWORD)null, null);
+#endif
 }
 
 void print_rune(rune character) {
-	std_syscall_write(0, &character, std_rune_bytes_used(character));
+#ifdef STD_PLATFORM_POSIX
+	std_posix_syscall_write(0, &character, std_rune_bytes_used(character));
+#elif defined(STD_PLATFORM_WINDOWS)
+	WriteConsoleA(
+		GetStdHandle(STD_WINDOWS_STD_OUTPUT_HANDLE),
+		&character,
+		std_rune_bytes_used(character),
+		(std_LPDWORD)null,
+		null
+	);
+#endif
 }
 
 void print_string(std_String string) {
-	std_syscall_write(0, string.data, string.length);
+#ifdef STD_PLATFORM_POSIX
+	std_posix_syscall_write(0, string.data, string.length);
+#elif defined(STD_PLATFORM_WINDOWS)
+	WriteConsoleA(GetStdHandle(STD_WINDOWS_STD_OUTPUT_HANDLE), string.data, string.length, (std_LPDWORD)null, null);
+#endif
 }
 
 void print_cstring(const char* cstring) {
@@ -45,7 +72,11 @@ void print_cstring(const char* cstring) {
 		string_length++;
 	}
 
-	std_syscall_write(0, cstring, string_length);
+#ifdef STD_PLATFORM_POSIX
+	std_posix_syscall_write(0, cstring, string_length);
+#elif defined(STD_PLATFORM_WINDOWS)
+	WriteConsoleA(GetStdHandle(STD_WINDOWS_STD_OUTPUT_HANDLE), cstring, string_length, (std_LPDWORD)null, null);
+#endif
 }
 
 #ifdef STD_LANGUAGE_CPP
