591e3d25771b4364c42013b0b9204725595a4019
BlackHole00 <lastapocalypse9@gmail.com>
2025-02-06 17:10:41 +0100

    STD: Fix windows build

--- std/src/_build.c
+++ std/src/_build.c
@@ -1,6 +1,8 @@
 #include "entrypoint_libc.c"
 #include "entrypoint_nolibc.c"
 
+#include "runtime_tcc.c"
+
 #include "runes.c"
 
 #include "slice.c"
@@ -8,5 +10,6 @@
 
 #include "page.c"
 #include "page_darwin.c"
+#include "page_windows.c"
 #include "allocator.c"
 
